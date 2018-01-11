eaa843164e12d991534b4648640028d7c7181875
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-01-11 07:59:02 -0700

    Use extension on the method-level and move code into test class

--- src/test/java/com/bmuschko/test/comparison/junit5/ConditionalExecutionTest.java
+++ src/test/java/com/bmuschko/test/comparison/junit5/ConditionalExecutionTest.java
@@ -2,9 +2,15 @@ package com.bmuschko.test.comparison.junit5;
 
 import com.bmuschko.test.comparison.ArithmeticOperation;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ConditionEvaluationResult;
+import org.junit.jupiter.api.extension.ExecutionCondition;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.extension.ConditionEvaluationResult.disabled;
+import static org.junit.jupiter.api.extension.ConditionEvaluationResult.enabled;
 
 public class ConditionalExecutionTest {
 
@@ -16,5 +22,25 @@ public class ConditionalExecutionTest {
     void testOnlyOnSystemSystemPropertySet() {
         assumeTrue(SYS_PROP_TRUE_VALUE.equals(System.getProperty(SYS_PROP_KEY)));
         assertEquals(3, arithmeticOperation.add(1, 2));
+    }
+
+    @ExtendWith(SystemPropertyConditionalExtension.class)
+    @Test
+    void testOnlyOnSystemSystemPropertySetByExtension() {
+        assertEquals(3, arithmeticOperation.add(1, 2));
+    }
+
+    private static class SystemPropertyConditionalExtension implements ExecutionCondition {
+
+        @Override
+        public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext context) {
+            boolean enabled = SYS_PROP_TRUE_VALUE.equals(System.getProperty(SYS_PROP_KEY));
+
+            if (enabled) {
+                return enabled(String.format("System property '%s' evaluates to true", SYS_PROP_KEY));
+            }
+
+            return disabled(String.format("System property '%s' evaluates to false", SYS_PROP_KEY));
+        }
     }
 }
